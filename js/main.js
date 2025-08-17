a5358937754a2a7cd64d95b929c02f9c68c239f5
Bismark Agyei <iamkofiagyei@gmail.com>
2025-08-17 08:35:47 +0000

    first push of code for my portfolio website

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const currentPage = window.location.pathname.split("/").pop();
+document.querySelectorAll("nav a").forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
